fedabd93615c2de555395097f92568c135d6239b
Brendan Darnell <brendan_darnell@hotmail.com>
2018-09-02 19:45:15 -0600

    Create server.js and blog-posts.js files

---
+++ blog-posts.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+
+const {BlogPosts} = require('./models');
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
